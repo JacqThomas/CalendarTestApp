ba2559e575b6e612be5c6f71b7c8cd2703f8a417
JacqThomas <jacqueline.thomas@sunsetcoast.capital>
2023-07-03 20:48:11 -0400

    Add project files.

---
+++ CalendarTest/Controllers/CalendlyController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalendarTest.Controllers
+{
+    public class CalendlyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ScheduleEvent()
+        {
+            return View();
+        }
+    }
+}
