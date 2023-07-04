847dd48db0dbf9af6b08d4e2b5ef2c767bbf2097
JacqThomas <jacqueline.thomas@sunsetcoast.capital>
2023-07-04 08:52:13 -0400

    moving the javascript around for better code reading

---
+++ CalendarTest/wwwroot/js/calendly-widget.js
@@ -0,0 +1,10 @@
+// calendly-widget.js
+(function () {
+    Calendly.initInlineWidget({
+        url: 'https://calendly.com/stackinglegos/demo-chat',
+        parentElement: document.getElementById('calendly-container'),
+        prefill: {},
+        utm: {}
+    });
+})();
+
